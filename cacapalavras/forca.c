f0231950302556b87dfaaad98fb84d5dd50bfa27
victor-hugo-da-silva-lima <48520646+victor-hugo-da-silva-lima@users.noreply.github.com>
2019-10-15 13:59:19 -0300

    Add files via upload

--- cacapalavras/forca.c
+++ cacapalavras/forca.c
@@ -1,71 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "forca.h"
 #include <string.h>
+#include "forca.h"
 
-
-struct noSecreto {
-int status;
-char palavra[31];
-char assunto [255];
-struct noSecreto * prox;
-};
+//gera um numero aleatorio entre 1 e 'maximo'
+int geraAleatorio(int maximo)
+{
+    time_t t;
+    int n=0;
+    srand((unsigned) time(&t));
+    n = (rand() % maximo)+1;
+    return n;
+}
 
 NoSecreto * inicializaListaSecreta()
 {
     return NULL;
 }
 
-NoSecreto * carregaListaArquivo(NoSecreto * l,char nomeArq [255])
-{
-    int status=0;
-    char palavra[31];
-    char assunto [255];
-
-
-    FILE * fWords;
-    fWords = fopen(nomeArq,"r");
-    if (fWords == NULL)
-    {
-      printf("Falha ao acessar base de dados!!! \n\n");
-      exit(0);
-    }
-    while (!feof(fWords))
-    {
-            fscanf(fWords,"%30[^- ]",palavra);
-    fscanf(fWords,"%99[^-\n]",assunto);
-
-    fscanf(fWords, "%*[-\n]");
-        l = inserePalavraSecreta(l,palavra,assunto);
-    }
-     fclose(fWords);
-    return  l;
-}
-
 NoSecreto * inserePalavraSecreta(NoSecreto *l,char word[31],char subject[100])
 {
-
- NoSecreto * novo;
- novo = (NoSecreto *) malloc(sizeof(NoSecreto));
- strcpy(novo->assunto,subject);
- strcpy(novo->palavra,word);
- novo->status=0;
- novo->prox = l;
- l=novo;
- return l;
-
+    NoSecreto * novo;
+    novo = (NoSecreto *) malloc(sizeof(NoSecreto));
+    novo->status=0;
+    strcpy(novo->assunto,subject);
+    strcpy(novo->palavra,word);
+    novo->prox = l;
+    l=novo;
+    return l;
 }
 
-void impimeListaSecreta(NoSecreto *l)
+void imprimeListaSecreta(NoSecreto *l)
 {
-    for (NoSecreto * p=l;p!=NULL;p=p->prox)
+    for(NoSecreto *p=l; p!=NULL; p=p->prox)
     {
-        printf("%0d | %5s | %5s |\n",p->status,p->palavra,p->assunto);
+        printf("%d) %-30s\t%s\n", p->status,p->palavra,p->assunto);
+    }
+}
+
+NoSecreto * carregaListaArquivo(NoSecreto * l, char nomeArq[255])
+{
+    const char ch[2]=";";
+    char * token;
+    int status;
+    char palavra[31];
+    char assunto[101];
+    char * linha[1024];
+    FILE * fWords;
+
+    fWords = fopen(nomeArq,"r");
+    if(fWords==NULL){
+        printf("Falha ao acessar base de dados!!!\n\n");
+        exit(0);
+    }
+    while(fgets(linha,1024,fWords))
+    {
+        token = strtok(linha,ch);
+        if(token!=NULL)
+        {
+             strcpy(palavra,token);
+        }
+        token=strtok(NULL,ch);
+        if(token!=NULL)
+        {
+            strcpy(assunto,token);
+        }
+        l = inserePalavraSecreta(l,palavra, assunto);
     }
 
+    fclose(fWords);
+    return l;
 }
 
+//
+int tamanhoListaSecreta(NoSecreto *l)
+{
+    int n=0;
+    for(NoSecreto *p=l;p!=NULL;p=p->prox,n++);
+    return n;
+}
 
+int temPalavraNaoUsada(NoSecreto *l)
+{
+    NoSecreto *p;
+    for(p=l;p!=NULL;p=p->prox){
+        if(p->status==0) return 1;
+    }
+    return 0;
+}
+
+NoSecreto * sorteiaPalavra(NoSecreto *l)
+{
+    int tam = tamanhoListaSecreta(l);
+    int aleatorio=0;
+    int tentativas = 0;
+    NoSecreto * p;
+
+    while(temPalavraNaoUsada(l)){
+        aleatorio = geraAleatorio(tam);
+        p = retornaPalavraPos(l,aleatorio);
+        if(p->status==0){
+            p->status = 1;
+            return p;
+        }
+    }
+    return NULL;
+}
+
+//retorna a palavra que está na posicao 'pos' da lisata secreta
+NoSecreto * retornaPalavraPos(NoSecreto *l, int pos)
+{
+    int n=1;
+    NoSecreto *p;
+    for(p=l; p!=NULL; p=p->prox)
+    {
+        if(n==pos) break;
+        n++;
+    }
+    return p;
+}
 
 
 
