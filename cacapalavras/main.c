2fde159743edbba403470940e8590e009519346f
gustavopoffo <48762069+gustavopoffo@users.noreply.github.com>
2019-11-11 19:28:30 -0300

    Atualizando versao 2

--- cacapalavras/main.c
+++ cacapalavras/main.c
@@ -1,57 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "forca.c"
 #include "forca.h"
-
-void copyright(){
-    system("clear");
-    printf("=============================================================\n\n");
-    printf("JOGO DA FORCA\n");
-    printf("Desenvolvido por: \n\tXXXXX - NNNNNNNNNNNNNNNNNNN\n");
-    printf("\tXXXXX - NNNNNNNNNNNNNNNNNNN\n");
-    printf("=============================================================\n\n");
+void exibe()
+{
+    printf("Desenvolvido por:\n Victor Hugo da Silva Lima 18153 e Gustavo de Araujo Poffo 18132");
+   
 }
 
-int limpaTela()
+int main()
 {
-puts("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-return 0;
-}
+    char nome[20];
+    printf("Digite seu nome:\n");
+    gets(nome);
 
-int main(int argc, char *argv[])
-{
+    char aux;
+    int help = 0;
+    int a = 0;
+    int b = 0;
+    int n = 0;
+    int cont = 0;
+    char letra;
+do{
+     system("cls");
+     printf("Bem vindo ao jogo %s\n",nome);
+     printf("Jogar - 1\n");
+     printf("Sair - 0\n");
+     scanf("%d",&a);
+    fflush(stdin);
+      if(a == 1)
+      {
+       b = 0;
+    exibe();
+    int x;
     NoSecreto * lstSecreta = inicializaListaSecreta();
-    char fNameArq[255];
     NoSecreto * sorteada;
-    int posSorteada=0;
-    int tamanhoLista=0;
-
-    strcpy(fNameArq,"palavras.dat");
-
-    if(argc>1){
-      strcpy(fNameArq,argv[1]);
-    }
-
-    printf("Arquivo de dados: %s\n\n\n",fNameArq);
-
-    copyright();
-
-    lstSecreta = carregaListaArquivo(lstSecreta,fNameArq);
+    char  aux1[31];
+    NoSecreto * usadas = inicializaListaSecreta();
+    lstSecreta = carregaListaArquivo(lstSecreta,"palavras.dat");
     imprimeListaSecreta(lstSecreta);
-    tamanhoLista = tamanhoListaSecreta(lstSecreta);
 
-    do{
-        CLEAR_SCREEN;
-        imprimeListaSecreta(lstSecreta);
+      do{
+        cont = 0;
+        system("cls");
+
+
 
         sorteada= sorteiaPalavra(lstSecreta);
+
+
         if(sorteada!=NULL){
+                strcpy(aux1,sorteada->palavra);
+            n = conta(sorteada->palavra);
+
+            for(int i = 0; i<n; i++)
+            {
+                    sorteada->palavra[i] = '*';
+            }
+            for(int x = 0; x<n*3; x++){
+
+                    help = 0;
+           if(strcmp(aux1,sorteada->palavra) == 0)
+                {
+                    printf("\nVoce achou a palavra\n");
+                    help = 1;
+                    break;
+                }
+                else{
+                        cont= cont+1;
+            exibe();
             printf("%s\n", sorteada->palavra);
-        }else{
-            printf("Não tem palavra disponível!\n\n");
+
+            printf("Digite uma letra:\n");
+
+            scanf("%c",&letra);
+
+            system("cls");
+
+            fflush(stdin);
+
+            for(int i = 0; i<n; i++)
+                {
+                if(aux1[i] == letra)
+                {
+                    sorteada->palavra[i] = aux1[i];
+                }
+
+            }
+                }
+            }
+            if(help == 1)
+            {
+            system("cls");
+            printf("\nEncontrou a palavra em %d tentaivas\n",cont);
+            printf("Escolhendo nova palavra........\n");
+            }
+            else{
+            system("cls");
+            printf("\nEsgotou tentativas\n");
+            printf("Numero de tentativas: %d\n",cont);
+            printf("Escolhendo nova palavra...\n");
+            }
+
+            lstSecreta = retiraUmElemento(lstSecreta,sorteada->palavra);
+            aux = lstSecreta;
         }
-    }while(getchar()!='f');
+        else{
+            printf("Nao existe palavra disponivel!\n\n");
+            b = 1;
+
+
+
+          }
+       }while(getchar()!='f' && b == 0);
+
+      }
+
+
+
+
+}while(a != 0);
+
 
 
     return 0;
